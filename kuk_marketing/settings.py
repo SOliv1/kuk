ef71b6fbe82e22000911d040a182f5762a577242
Keval Kant <kevalkant9098@gmail.com>
2021-08-13 22:52:48 +0530

    bucket name changed

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -194,7 +194,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'kuk-milestone-4'
+    AWS_STORAGE_BUCKET_NAME = 'kuk-marketing'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
