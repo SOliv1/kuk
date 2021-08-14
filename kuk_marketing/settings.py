780642dce08ba4ae2ad5f22f8e6b745ac9f6baa6
ww004050 <kevalkant9098@gmail.com>
2021-08-14 23:26:17 +0530

    adobe video

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'None'
+    #AWS_DEFAULT_ACL = 'None'
 
     # media and Static  files
     #STATICFILES_STORAGE = 'custom_storages.StaticStorage'
