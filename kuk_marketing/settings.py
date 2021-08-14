35730b13862ad8aadcec174bd7e1f7cab3618644
ww004050 <kevalkant9098@gmail.com>
2021-08-14 20:58:27 +0530

    allowed host modified

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "my_secret_key")
 # DEBUG = False
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['kuk-marketing.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['kuk-marketing.herokuapp.com', 'localhost', '127.0.0.1', 'www.kukmarketing.com']
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    # AWS_DEFAULT_ACL = 'None'
+    AWS_DEFAULT_ACL = 'None'
 
     # media and Static  files
     #STATICFILES_STORAGE = 'custom_storages.StaticStorage'
