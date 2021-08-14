4d593632b66b06f0087563ad551aa56d97599212
ww004050 <kevalkant9098@gmail.com>
2021-08-14 23:49:41 +0530

    changes

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -237,6 +237,6 @@ else:
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
     
