5d581f3d04a2ba8c6e8e371ab085a3cdb9d76db0
ww004050 <kevalkant9098@gmail.com>
2021-08-15 12:06:02 +0530

    database changed to heroku postgres

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -131,18 +131,19 @@ WSGI_APPLICATION = 'kuk_marketing.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validate
