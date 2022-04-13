a64e5aaaa12d16ffaf4c84c532f3c5f682475810
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2022-04-13 23:14:04 +0100

    Update settings.py

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -131,20 +131,18 @@ WSGI_APPLICATION = 'kuk_marketing.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
 
 # Password validate
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
