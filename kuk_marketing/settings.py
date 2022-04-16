f919c07949d1e0628c2f2d5b100d49c3fe0d026c
keval9098 <kevalkant9098@gmail.com>
2022-04-16 20:03:26 +0530

    404 page

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "my_secret_key")
 #    development = True
 # else:
 # DEBUG = False
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['kuk-marketing.herokuapp.com', 'localhost', '127.0.0.1', 'www.kukmarketing.com']
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
